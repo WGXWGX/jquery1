f82934ff32f5801b3f933571eb922d246d43f2b6
WGXWGX <wgx15145076862@sina.com>
2016-08-01 22:22:18 +0800

    jq2
    引入的index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+$(function(){
+	var $td = $('.td');
+	$('tr:even').not('.thead').css({
+		background: 'red'
+	});
+	$td.on('click',function(){
+		if($(this).children('input').length > 0){
+			return false;
+		}
+		var $input = $('<input type="text" value='+this.innerHTML+'>').css({
+			width: $(this).width(),
+			border: 0,
+			background: 'red'
+		});
+		$(this).empty().append($input);
+		//console.log(this.innerHTML);
+		$input.trigger('select');
+		// if($td.children('input').length > 0){
+		// 	return false;
+		// }
+	});
+	
+
+});
